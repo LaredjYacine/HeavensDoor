6867e4f6d45c8c372130fe375534d7e9caa1f0c8
LaredjYacine <laredjyacine955@gmail.com>
2026-09-11 19:30:58 +0100

    feat:added bmSearch in Supabase

--- src/heavensdoor/app/services/Search_function.py
+++ src/heavensdoor/app/services/Search_function.py
@@ -5,9 +5,12 @@ def rag_function(query_vector:str):
     vector_threshold=0.75
     match_count = 5
     dataset = supabase_client.rpc('cosine_similarity',{'query_vector':embedded_query.tolist() ,'vector_threshold': vector_threshold, 'match_count': match_count, }).execute()
-    for data in dataset.data :
-        print(data['id'])
-
+    return dataset.data
 
 
 rag_function('Software Engineer')
+
+
+
+def bm25_function(query_vector:str):
+    pass ## i might do a supabase Search rather than bm25 !
