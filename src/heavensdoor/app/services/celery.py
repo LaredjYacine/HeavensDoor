10a0bb0601a86a180b8860cd82234b2b6d882fac
LaredjYacine <laredjyacine955@gmail.com>
2026-09-19 17:01:02 +0100

    fix : added include to the celery

--- src/heavensdoor/app/services/celery.py
+++ src/heavensdoor/app/services/celery.py
@@ -2,4 +2,4 @@ from   celery import   Celery
 import dotenv
 import os
 from ..services.credentials import redisurl
-celery_app = Celery('ai_agent', broker=redisurl, backend=redisurl,worker_pool="solo")
+celery_app = Celery('ai_agent', broker=redisurl, backend=redisurl,worker_pool="solo",  include=['heavensdoor.app.routes.background_Process'])
